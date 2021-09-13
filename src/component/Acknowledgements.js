38a56ff72c8b3b24058c321066f522e596c1757b
Benjamin Yap <benjamin25116@users.noreply.github.com>
2021-09-13 11:53:49 +0800

    Update acknowledgements section

--- src/component/Acknowledgements.js
+++ src/component/Acknowledgements.js
@@ -6,35 +6,43 @@ import Heading from "./Heading";
 function Acknowledgements() {
   return (
     <Layout backgroundColor="#f9f4f1">
-      <Heading>Acknowledgement</Heading>
+      <Heading>A Note of Thanks</Heading>
       <p>
-        The past year has been a difficult for many. It is organizations like
-        PERKAMA and Malaysia Association for Social Workers that hold up beacons
-        of hope and encouragement to many of us. We want to thank them for their
-        support and backing. Your partnership was not only in words but in
-        deeds. We are grateful. Thank you.
+        The past year has been a difficult one for many mental health and social
+        welfare organizations. Our interactions with our sponsors, PERKAMA and
+        Malaysia Association for Social Workers, never failed to encourage us.
+        We want to thank Dato’ Dr. Halim Hussin, Puan Amy Bala and Dr. Azlinda
+        Azman, for their strong and steady support during the planning of the
+        symposium. Your warmth, kindness, enthusiasm and words of wisdom, saw us
+        through many challenges. Your partnership with us was not only in words,
+        it was followed through in actions. We are very grateful. Your
+        organizations are beacons of hope and encouragement to many of us.
       </p>
       <p>
-        To the team at Dignity who worked many hours behind the scenes in order
-        to make this symposium a success, we offer our thanks to you. Without
-        you this symposium would have remained in our minds but you took our
-        ideas and thoughts and translated them into this event. How can we thank
-        you enough. But we will try - thank you.
+        To Rev Elisha Satvinder and Petrina Satvinder: From the very conception
+        of Dignity, your pioneering spirit has greeted every new challenge and
+        endeavor with a big “YES” and a “Let's do it”. You epitomize the phrase,
+        “To boldly go where no man has gone before…" (Dillard, J). Thank you for
+        saying a big YES to this symposium.
       </p>
       <p>
-        To Rev. Elisha Satvinder and Petrina Satvinder, your pioneering spirit
-        greets each new adventure with a big YES let's do it. Thank you for
-        saying YES to this symposium.
+        It would be easy to forget the most important contributors to this
+        event. Behind the scenes, a group has worked in order to make this
+        symposium a success. To each staff member on this team, the deepest
+        thank you and gratitude are due to you for so willingly putting in your
+        hard work, time and tireless energy. Without you, this symposium would
+        merely have remained in the mind, but you took the thoughts and ideas
+        and translated them into this event.
       </p>
       <p>
-        Sai Fun, Dignity has adopted you as part of our staff. Why won't we: you
-        answer our questions asked at absurd hours of the night; you limp,
-        squint and squeal because you forgot that you needed to take a break to
-        relax, give your eyes some rest and take more washroom breaks 🤣. There
-        is only one of you in a million (perhaps two millions). And you are a
-        brilliant copywriter to boot. We got ourselves a great partnership when
-        you came aboard. Thank you for all the hours given sacrificially without
-        complaint and reimbursement.
+        Sai Fun, Dignity has adopted you as one of its staff. Why wouldn’t we?
+        You answered our questions, responded to requests for help at absurd
+        hours of the day and night; you limped, squinted and squealed because
+        you forgot that you needed to take a break to relax, to give your eyes
+        some rest and to take more washroom breaks 🤣. We got ourselves a great
+        partnership when you 'came aboard' and a brilliant copywriter to boot.
+        We owe you an unrepayable debt. Thank you is not big enough a word to
+        express our gratitude for your work.
       </p>
     </Layout>
   );
