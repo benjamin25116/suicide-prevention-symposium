c34fef0f2928dbfe2a8c67807a388243dc5b6f7f
Benjamin Yap <benjamin25116@users.noreply.github.com>
2021-09-13 14:17:26 +0800

    Add google tag and analytics

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import * as React from "react";
 import GlobalStyle from "./GlobalStyle";
+import { Helmet } from "react-helmet";
 
 import Layout from "../component/Layout";
 import HeroBanner from "../component/HeroBanner";
@@ -19,6 +20,35 @@ const IndexPage = () => {
   return (
     <main>
       <GlobalStyle />
+      <Helmet>
+        <title>Suicide: The Silent Pandemic</title>
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-HXGRMMZXNP"
+        ></script>
+        <script>
+          {`window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-HXGRMMZXNP');`}
+        </script>
+        <script>{`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-W736F9X');`}</script>
+      </Helmet>
+
+      <noscript>
+        <iframe
+          title="google tag manager"
+          src="https://www.googletagmanager.com/ns.html?id=GTM-W736F9X"
+          height="0"
+          width="0"
+          style={{ display: "none", visibility: "hidden" }}
+        ></iframe>
+      </noscript>
 
       <HeroBanner />
       <Layout backgroundColor="#f9f4f1">
