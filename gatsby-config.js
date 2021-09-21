50195bfe2f68534ca704a1ce91ae99c92c5686f0
Benjamin Yap <benjamin25116@users.noreply.github.com>
2021-09-21 09:36:38 +0800

    Add meta tags.

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,18 +4,6 @@ module.exports = {
     title: "Suicide: The Silent Pandemic",
     description:
       "Malaysia's First Multidisciplinary Symposium on Suicide Prevention.",
-    image:
-      "https://silentpandemic.netlify.app/static/b5fc3d3190a6dfb137e57905291edf11/39d9b/fahad-bin-kamal-anik-jNS7kDxkIyQ-unsplash.webp",
-    og: {
-      title: "Suicide: The Silent Pandemic",
-      description:
-        "Malaysia's First Multidisciplinary Symposium on Suicide Prevention.",
-      image:
-        "https://silentpandemic.netlify.app/static/b5fc3d3190a6dfb137e57905291edf11/39d9b/fahad-bin-kamal-anik-jNS7kDxkIyQ-unsplash.webp",
-      url: "https://silentpandemic.netlify.app",
-      site_name: "Suicide: The Silent Pandemic",
-      type: "website",
-    },
   },
   plugins: [
     "gatsby-plugin-styled-components",
--- src/pages/index.js
+++ src/pages/index.js
@@ -16,7 +16,7 @@ import Charges from "../component/Charges";
 
 function IndexPage() {
   return (
-    <>
+    <main>
       <Helmet>
         <meta charset="utf-8" />
         <title>Suicide: The Silent Pandemic</title>
@@ -37,55 +37,53 @@ function IndexPage() {
           itemprop="image"
           content="https://silentpandemic.netlify.app/static/b5fc3d3190a6dfb137e57905291edf11/39d9b/fahad-bin-kamal-anik-jNS7kDxkIyQ-unsplash.webp"
         />
-        <meta name="og:title" content="Suicide: The Silent Pandemic" />
+        <meta property="og:title" content="Suicide: The Silent Pandemic" />
         <meta
-          name="og:description"
+          property="og:description"
           content="Malaysia's First Multidisciplinary Symposium on Suicide Prevention."
         />
         <meta
-          name="og:image"
+          property="og:image"
           content="https://silentpandemic.netlify.app/static/b5fc3d3190a6dfb137e57905291edf11/39d9b/fahad-bin-kamal-anik-jNS7kDxkIyQ-unsplash.webp"
         />
-        <meta name="og:url" content="https://silentpandemic.netlify.app/" />
-        <meta name="og:site_name" content="Suicide: The Silent Pandemic" />
-        <meta name="og:type" content="website" />
+        <meta property="og:url" content="https://silentpandemic.netlify.app/" />
+        <meta property="og:site_name" content="Suicide: The Silent Pandemic" />
+        <meta property="og:type" content="website" />
       </Helmet>
-      <main>
-        <GlobalStyle />
-        <noscript>
-          <iframe
-            title="google tag manager"
-            src="https://www.googletagmanager.com/ns.html?id=GTM-W736F9X"
-            height="0"
-            width="0"
-            style={{ display: "none", visibility: "hidden" }}
-          ></iframe>
-        </noscript>
+      <GlobalStyle />
+      <noscript>
+        <iframe
+          title="google tag manager"
+          src="https://www.googletagmanager.com/ns.html?id=GTM-W736F9X"
+          height="0"
+          width="0"
+          style={{ display: "none", visibility: "hidden" }}
+        ></iframe>
+      </noscript>
 
-        <HeroBanner />
-        <MainCopy />
-        <SplitBanner
-          src={FirstImage}
-          heading="Who Should Attend"
-          body={[
-            "Anyone who seeks to stand up and acknowledge our responsibility in perpetuating the stigma and silence of suicide.",
-            "Anyone who is concerned about the increasing number of suicides and suicide attempts in the country and wants to help do something to prevent suicide.",
-          ]}
-        />
-        <SplitBanner
-          src={SecondImage}
-          heading="Why Attend"
-          body="Because preventing suicide is everyone’s responsibility. You may be able to save a life if you’re more informed about the many faces of suicide. Come be a part of something that can literally make a life or death difference."
-        />
-        <Charges />
-        <Schedule />
-        <Presenters />
-        <Contact />
-        <Acknowledgements />
-        <hr />
-        <Footer />
-      </main>
-    </>
+      <HeroBanner />
+      <MainCopy />
+      <SplitBanner
+        src={FirstImage}
+        heading="Who Should Attend"
+        body={[
+          "Anyone who seeks to stand up and acknowledge our responsibility in perpetuating the stigma and silence of suicide.",
+          "Anyone who is concerned about the increasing number of suicides and suicide attempts in the country and wants to help do something to prevent suicide.",
+        ]}
+      />
+      <SplitBanner
+        src={SecondImage}
+        heading="Why Attend"
+        body="Because preventing suicide is everyone’s responsibility. You may be able to save a life if you’re more informed about the many faces of suicide. Come be a part of something that can literally make a life or death difference."
+      />
+      <Charges />
+      <Schedule />
+      <Presenters />
+      <Contact />
+      <Acknowledgements />
+      <hr />
+      <Footer />
+    </main>
   );
 }
 
