91451ce0c17b25e09f33e94b0a37f0cab328020e
Benjamin Yap <benjamin25116@users.noreply.github.com>
2021-09-21 09:10:35 +0800

    Update meta tags in site metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,21 @@
 module.exports = {
   siteMetadata: {
-    siteUrl: "https://https://silentpandemic.netlify.app/",
+    siteUrl: "https://silentpandemic.netlify.app/",
     title: "Suicide: The Silent Pandemic",
     description:
       "Malaysia's First Multidisciplinary Symposium on Suicide Prevention.",
+    image:
+      "https://silentpandemic.netlify.app/static/b5fc3d3190a6dfb137e57905291edf11/39d9b/fahad-bin-kamal-anik-jNS7kDxkIyQ-unsplash.webp",
+    og: {
+      title: "Suicide: The Silent Pandemic",
+      description:
+        "Malaysia's First Multidisciplinary Symposium on Suicide Prevention.",
+      image:
+        "https://silentpandemic.netlify.app/static/b5fc3d3190a6dfb137e57905291edf11/39d9b/fahad-bin-kamal-anik-jNS7kDxkIyQ-unsplash.webp",
+      url: "https://silentpandemic.netlify.app",
+      site_name: "Suicide: The Silent Pandemic",
+      type: "website",
+    },
   },
   plugins: [
     "gatsby-plugin-styled-components",
