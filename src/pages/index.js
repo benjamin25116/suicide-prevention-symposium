56bbe97dccab5833e0cef4f68632f7dfa9172f40
Benjamin Yap <benjamin25116@users.noreply.github.com>
2021-09-21 08:24:01 +0800

    Add meta tags for FB.

--- src/pages/index.js
+++ src/pages/index.js
@@ -15,55 +15,68 @@ import SecondImage from "../images/alex-ivashenko-RLdcScGQEJ4-unsplash.jpg";
 import Charges from "../component/Charges";
 
 function IndexPage() {
-	return (
-		<main>
-			<GlobalStyle />
-			<Helmet>
-				<title>Suicide: The Silent Pandemic</title>
-				<meta
-					name="description"
-					content="Malaysia's First Multidisciplinary Symposium on Suicide Prevention."
-				/>
-				{/* <script
+  return (
+    <main>
+      <GlobalStyle />
+      <Helmet>
+        <title>Suicide: The Silent Pandemic</title>
+        <meta
+          name="description"
+          content="Malaysia's First Multidisciplinary Symposium on Suicide Prevention."
+        />
+        <meta
+          property="og:image"
+          content="https://silentpandemic.netlify.app/static/b5fc3d3190a6dfb137e57905291edf11/39d9b/fahad-bin-kamal-anik-jNS7kDxkIyQ-unsplash.webp"
+        />
+        <meta property="og:title" content="Suicide: the Silent Pandemic." />
+        <meta
+          property="og:description"
+          content="A multidisciplinary symposium on suicide prevention."
+        />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://silentpandemic.netlify.app" />
+
+        {/* og:url, og:type, og:title, og:image, og:description, fb:app_id */}
+        {/* <script
           async
           src="https://www.googletagmanager.com/gtag/js?id=G-HXGRMMZXNP"
         ></script> */}
-			</Helmet>
+      </Helmet>
 
-			<noscript>
-				<iframe
-					title="google tag manager"
-					src="https://www.googletagmanager.com/ns.html?id=GTM-W736F9X"
-					height="0"
-					width="0"
-					style={{ display: "none", visibility: "hidden" }}
-				></iframe>
-			</noscript>
-			
-			<HeroBanner />
-			<MainCopy />
-			<SplitBanner
-				src={FirstImage}
-				heading="Who Should Attend"
-				body={[
-					"Anyone who seeks to stand up and acknowledge our responsibility in perpetuating the stigma and silence of suicide.",
-					"Anyone who is concerned about the increasing number of suicides and suicide attempts in the country and wants to help do something to prevent suicide.",
-				]}
-			/>
-			<SplitBanner
-				src={SecondImage}
-				heading="Why Attend"
-				body="Because preventing suicide is everyone’s responsibility. You may be able to save a life if you’re more informed about the many faces of suicide. Come be a part of something that can literally make a life or death difference."
-			/>
-			<Charges />
-			<Schedule />
-			<Presenters />
-			<Contact />
-			<Acknowledgements />
-			<hr />
-			<Footer />
-		</main>
-	);
+      <noscript>
+        <iframe
+          title="google tag manager"
+          src="https://www.googletagmanager.com/ns.html?id=GTM-W736F9X"
+          height="0"
+          width="0"
+          style={{ display: "none", visibility: "hidden" }}
+        ></iframe>
+      </noscript>
+
+      <HeroBanner />
+      <MainCopy />
+      <SplitBanner
+        src={FirstImage}
+        heading="Who Should Attend"
+        body={[
+          "Anyone who seeks to stand up and acknowledge our responsibility in perpetuating the stigma and silence of suicide.",
+          "Anyone who is concerned about the increasing number of suicides and suicide attempts in the country and wants to help do something to prevent suicide.",
+        ]}
+      />
+      <SplitBanner
+        src={SecondImage}
+        heading="Why Attend"
+        body="Because preventing suicide is everyone’s responsibility. You may be able to save a life if you’re more informed about the many faces of suicide. Come be a part of something that can literally make a life or death difference."
+      />
+      <Charges />
+      <Schedule />
+      <Presenters />
+      <Contact />
+      <Acknowledgements />
+      <hr />
+      <Footer />
+    </main>
+  );
 }
 
 export default IndexPage;
